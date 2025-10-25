c42cc660202a4937d7a15340e8fdc8f451a423f9
andrids396 <126834617+andrids396@users.noreply.github.com>
2025-10-25 07:13:16 +0700

    Update main.py
    Sudo level easy to hard

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
+import sys
+#menampilkan sudo
 def fmenampilkan_sudo():
     for baris in sudo:
-        #print(baris)
         for nilai in sudo[baris].values():
             print(nilai,"",end="")
         print()
-
+# mencari nikai sebaris
 def fbaris(titik):
     baris = titik[0]
     nilai_baris = set([])
     for nilai in sudo[baris].values():
-        #print(nilai)
         nilai_baris.add(nilai)
     return nilai_baris
-    
+# mencari nilai sekolom
 def fkolom(titik):
     nilai_kolom = set([])
     kolom = titik[1]
@@ -20,7 +20,8 @@ def fkolom(titik):
         nilai = sudo[baris][kolom]
         nilai_kolom.add(nilai)
     return nilai_kolom
-    
+
+# mencari nilai seblok
 def fblok(titik):
     nilai_blok = set([])
     baris = titik[0]
@@ -45,6 +46,7 @@ def fblok(titik):
             nilai_blok.add(nilai)
     return nilai_blok
 
+# mengumpulkan nilai sudo
 def fnilai_sudo():
     nilai_sudo = set([])
     for i in sudo.values():
@@ -53,29 +55,30 @@ def fnilai_sudo():
     return nilai_sudo
 
 # soal sudo
-sudo = {1: {1: 0, 2: 2, 3: 0, 4: 4},
-        2: {1: 0, 2: 0, 3: 1, 4: 0},
-        3: {1: 2, 2: 1, 3: 0, 4: 3},
-        4: {1: 4, 2: 0, 3: 0, 4: 1}
+sudo = {1: {1: 3, 2: 0, 3: 1, 4: 2},
+        2: {1: 0, 2: 0, 3: 0, 4: 0},
+        3: {1: 2, 2: 0, 3: 0, 4: 0},
+        4: {1: 0, 2: 0, 3: 0, 4: 4}
         }
         
 print("soal sudo 4x4:")
-#menampilkan sudo
+#menampilkan soal sudo
 fmenampilkan_sudo()
 
 # program utama
 nilai_lengkap = set([1, 2, 3, 4])
-nilai_sudo = fnilai_sudo()
-#print(nilai_sudo)
-# memeriksa apakah 0 ada di sudo
+
 while True:
+    # mengumpulkan nilai sudo
+    nilai_sudo = fnilai_sudo()
+    # dummy nikai kemungkinan terpendek (nilai kemungkinan utana) yang akan di update ke sudo
+    nilai_kemungkinan_utama = [1, 2, 3, 4, 5]
     #memeriksa apakah nilai 0 ada di sudo
     if 0 not in nilai_sudo:
         # jika tidak ada nilai 0 program berakhir
         print("\nJawaban: ")
         #menampilkan sudo
         fmenampilkan_sudo()
-
         break
     else:
         # jika ada nilai 0, cari titik (baris, kolom) bernilai 0
@@ -91,23 +94,22 @@ while True:
                     nilai_blok = fblok(titik)
                     # menggabungkan (union) nilai_baris, nilai_kolom dan nilai_blok
                     nilai_titik = nilai_baris | nilai_kolom | nilai_blok
-                    # mencaru kemungkinan nilai untuk nilai 0
+                    # mencari kemungkinan nilai untuk nilai 0
                     nilai_kemungkinan = nilai_lengkap - nilai_titik
                     # mengubah tipe data set menjadi list
                     nilai_kemungkinan = list(nilai_kemungkinan)
-                    nilai = nilai_kemungkinan[0]
-                    # jika panjang nilai_kemungkinan == 1, maka update sudo
-                    if len(nilai_kemungkinan) == 1:
-                        sudo[baris][kolom] = nilai
-                        nilai_sudo = fnilai_sudo()
-
-
-
-
-
-
-
-
-
-
-
+                    # mencari nilai kemungjinan terpendek
+                    if len(nilai_kemungkinan) < len(nilai_kemungkinan_utama):
+                        nilai_kemungkinan_utama = nilai_kemungkinan
+                        BARIS = baris
+                        KOLOM = kolom
+    # untuk menunjukan soal salah, dalam satu baris, kolom atau blok terdapat angka yang sama
+    if nilai_kemungkinan_utama == []:
+        print("\nJawaban sementara:")
+        sudo[BARIS][KOLOM] = "i"
+        fmenampilkan_sudo()
+        print(f"eror: angka imajiner di ({BARIS},{KOLOM})")
+        sys.exit()
+    # mengupdate sudo
+    nilai = nilai_kemungkinan_utama[0]
+    sudo[BARIS][KOLOM] = nilai
